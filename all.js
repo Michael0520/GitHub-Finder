84c157e449033679f3afe4a91ae78695f9cddd81
MichaelLuo520 <po2319987@gmail.com>
2021-03-19 04:05:07 +0800

    Display profile and test to input value

--- all.js
+++ all.js
@@ -1,3 +1,9 @@
+// Init GitHub
+const github = new Github();
+
+// Init UI
+const ui = new UI();
+
 // Search input
 const searchUser = document.getElementById("searchUser");
 
@@ -7,6 +13,17 @@ searchUser.addEventListener("keyup", (e) => {
   const userText = e.target.value;
 
   if (userText !== "") {
-    console.log(userText);
+    github.getUser(userText).then((data) => {
+      if (data.profile.message === "Not Found") {
+        // Show alert
+        ui.showAlert("User not found", "alert alert-danger");
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+      }
+    });
+  } else {
+    // Clear profile
+    ui.clearProfile();
   }
 });
